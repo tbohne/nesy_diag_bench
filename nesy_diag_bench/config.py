cc7d73b81c3d31eab322c717d801924ef1d5f483
tbohne <tim.bohne@dfki.de>
2024-02-12 11:01:11 +0100

    impl config

---
+++ nesy_diag_bench/config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author Tim Bohne
+
+FUSEKI_URL = "http://localhost:3030"
+DATASET_NAME = "nesy_diag"
+UPDATE_ENDPOINT = f"{FUSEKI_URL}/{DATASET_NAME}/update"
+DATA_ENDPOINT = f"{FUSEKI_URL}/{DATASET_NAME}/data"
