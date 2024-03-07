4c8d77887ec180abeaa6f9b3b9a491215c13596b
tbohne <tim.bohne@dfki.de>
2024-03-07 11:56:36 +0100

    round results in cumulative res

--- nesy_diag_bench/analyze_res.py
+++ nesy_diag_bench/analyze_res.py
@@ -42,30 +42,30 @@ if __name__ == '__main__':
         print("working on instance set:", instance_set_sol)
         df = pd.read_csv(instance_set_sol)
         num_of_instances = df.count()["instance"]
-        avg_tp = df["TP"].describe()["mean"]
-        avg_tn = df["TN"].describe()["mean"]
-        avg_fp = df["FP"].describe()["mean"]
-        avg_fn = df["FN"].describe()["mean"]
-        fp_dev_mean = df["#fp_dev"].describe()["mean"]
+        avg_tp = round(df["TP"].describe()["mean"], 2)
+        avg_tn = round(df["TN"].describe()["mean"], 2)
+        avg_fp = round(df["FP"].describe()["mean"], 2)
+        avg_fn = round(df["FN"].describe()["mean"], 2)
+        fp_dev_mean = round(df["#fp_dev"].describe()["mean"], 2)
         fp_dev_max = df["#fp_dev"].describe()["max"]
-        avg_accuracy = df["acc"].describe()["mean"]
-        avg_prec = df["prec"].describe()["mean"]
-        avg_rec = df["rec"].describe()["mean"]
-        avg_spec = df["spec"].describe()["mean"]
-        avg_f1 = df["F1"].describe()["mean"]
-        avg_ano_link_percentage = df["ano_link_perc"].describe()["mean"]
-        avg_model_acc = df["avg_model_acc"].describe()["mean"]
+        avg_accuracy = round(df["acc"].describe()["mean"], 2)
+        avg_prec = round(df["prec"].describe()["mean"], 2)
+        avg_rec = round(df["rec"].describe()["mean"], 2)
+        avg_spec = round(df["spec"].describe()["mean"], 2)
+        avg_f1 = round(df["F1"].describe()["mean"], 2)
+        avg_ano_link_percentage = round(df["ano_link_perc"].describe()["mean"], 2)
+        avg_model_acc = round(df["avg_model_acc"].describe()["mean"], 2)
 
         top_class = df["gt_match"].describe()["top"]
         top_freq = df["gt_match"].describe()["freq"]
         gt_match_percentage = top_freq if top_class else 100 - top_freq
 
-        avg_num_fault_paths = df["#fault_paths"].describe()["mean"]
+        avg_num_fault_paths = round(df["#fault_paths"].describe()["mean"], 2)
         max_num_fault_paths = df["#fault_paths"].describe()["max"]
-        avg_fault_path_len = df["avg_fp_len"].describe()["mean"]
+        avg_fault_path_len = round(df["avg_fp_len"].describe()["mean"], 2)
         max_fault_path_len = df["avg_fp_len"].describe()["max"]
 
-        avg_runtime = df["runtime (s)"].describe()["mean"]
+        avg_runtime = round(df["runtime (s)"].describe()["mean"], 2)
 
         write_instance_set_res_to_csv(
             instance_set_sol, num_of_instances, avg_tp, avg_tn, avg_fp, avg_fn, fp_dev_mean, fp_dev_max, avg_accuracy,
