04e09fc5a8bb83c219e514edbbc4ab3e03247189
tbohne <tim.bohne@dfki.de>
2024-03-18 15:37:42 +0100

    impl 2nd kind of compensation

--- analysis/meta_analysis.py
+++ analysis/meta_analysis.py
@@ -13,8 +13,13 @@ anomaly_perc_aff_by_ratio = [
 
 anomaly_link_perc_scores = [round(i / 100.0, 2) for i in df["avg_ano_link_perc"]]
 
-compensation = [
+compensation_ano_link = [
     round(anomaly_link_perc_scores[i] - df["avg_f1"][i], 2)
+    for i in range(len(df["avg_f1"]))
+]
+
+compensation_gtfp = [
+    round(df["avg_ratio_of_found_gtfp"][i] - df["avg_f1"][i], 2)
     for i in range(len(df["avg_f1"]))
 ]
 
@@ -22,15 +27,16 @@ with open("compensation.csv", mode='a', newline='') as csv_file:
     writer = csv.writer(csv_file)
     writer.writerow(
         ["anomaly_perc_aff_by_ratio", "f1_scores", "anomaly_link_perc_scores",
-        "compensation", "gt_match_perc", "avg_ratio_gtfp"]
+        "compensation_ano_link", "gt_match_perc", "avg_ratio_gtfp", "compensation_gtfp"]
     )
 
-    for i in range(len(compensation)):
+    for i in range(len(compensation_ano_link)):
         writer.writerow([
             anomaly_perc_aff_by_ratio[i],
             df["avg_f1"][i],
             anomaly_link_perc_scores[i],
-            compensation[i],
+            compensation_ano_link[i],
             df["gt_match_perc"][i],
-            df["avg_ratio_of_found_gtfp"][i]
+            df["avg_ratio_of_found_gtfp"][i],
+            compensation_gtfp[i]
         ])
