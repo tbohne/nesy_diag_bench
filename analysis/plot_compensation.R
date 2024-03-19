9488e39017a774fc05ec5aadfdb0d8c05d409b5a
tbohne <tim.bohne@dfki.de>
2024-03-19 17:50:20 +0100

    extend compensation plot gen

--- analysis/plot_compensation.R
+++ analysis/plot_compensation.R
@@ -5,13 +5,18 @@ PT_DEF <- geom_point(size=5)
 COLOR_VALS <- c("#d44345", "#ffb641", "#ffff00", "#ccff99", "#00ff00")
 
 gen_multi_plot_four <- function(
-        pp1, pp2, pp3, pp4, y1, y2, y3, y4, x1, x2, x3, x4, filename
+        pp1, pp2, pp3, pp4, pp5, pp6,
+        y1, y2, y3, y4, y5, y6,
+        x1, x2, x3, x4, x5, x6,
+        filename
     ) {
     fpp1 <- pp1 + PT_DEF + xlab(x1) + ylab(y1) + scale_color_manual(values = color_mapping)
     fpp2 <- pp2 + PT_DEF + xlab(x2) + ylab(y2) + scale_color_manual(values = color_mapping)
     fpp3 <- pp3 + PT_DEF + xlab(x3) + ylab(y3) + scale_color_manual(values = color_mapping)
     fpp4 <- pp4 + PT_DEF + xlab(x4) + ylab(y4) + scale_color_manual(values = color_mapping)
-    combined_plot <- grid.arrange(fpp1, fpp2, fpp3, fpp4, ncol = 2)
+    fpp5 <- pp5 + PT_DEF + xlab(x5) + ylab(y5) + scale_color_manual(values = color_mapping)
+    fpp6 <- pp6 + PT_DEF + xlab(x6) + ylab(y6) + scale_color_manual(values = color_mapping)
+    combined_plot <- grid.arrange(fpp1, fpp2, fpp3, fpp4, fpp5, fpp6, ncol = 3)
     ggsave(combined_plot, file = filename, width = 12, height = 6)
 }
 
@@ -70,15 +75,37 @@ p4 <- ggplot(
     )
 )
 
+p5 <- ggplot(
+    data = input, aes_string(
+        x = "compensation_gtfp",
+        y = "compensation_ano_link",
+        color = "gt_match",
+        group = "gt_match"
+    )
+)
+
+p6 <- ggplot(
+    data = input, aes_string(
+        x = "compensation_gtfp",
+        y = "anomaly_perc_aff_by_ratio",
+        color = "gt_match",
+        group = "gt_match"
+    )
+)
+
 gen_multi_plot_four(
-    p1, p2, p3, p4,
+    p1, p2, p3, p4, p5, p6,
     "avg_ratio_gtfp",
     "anomaly_perc_aff_by_ratio",
     "anomaly_perc_aff_by_ratio",
+    "anomaly_perc_aff_by_ratio",
+    "compensation_ano_link",
     "anomaly_perc_aff_by_ratio",
     "compensation_ano_link",
     "compensation_ano_link",
     "anomaly_link_perc_scores",
     "avg_ratio_gtfp",
+    "compensation_gtfp",
+    "compensation_gtfp",
     "compensation_analysis.png"
 )
