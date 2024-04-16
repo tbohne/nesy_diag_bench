4f3ec634b8bf2d8400506ee2ddfacfa9711586b9
tbohne <tim.bohne@dfki.de>
2024-04-16 11:08:38 +0200

    impl median metric for fault path len + num of fault paths

--- nesy_diag_bench/analyze_res.py
+++ nesy_diag_bench/analyze_res.py
@@ -14,7 +14,8 @@ def write_instance_set_res_to_csv(
         instance_set_sol, num_of_instances, avg_tp, avg_tn, avg_fp, avg_fn, fp_dev_mean, fp_dev_max, avg_accuracy,
         avg_prec, avg_rec, avg_spec, avg_f1, avg_ano_link_percentage, avg_model_acc, gt_match_percentage,
         avg_num_fault_paths, max_num_fault_paths, avg_fault_path_len, max_fault_path_len, filename, avg_runtime,
-        avg_classification_ratio, avg_ratio_of_found_gtfp, diag_success_percentage, median_runtime
+        avg_classification_ratio, avg_ratio_of_found_gtfp, diag_success_percentage, median_runtime,
+        median_num_fault_paths, median_fault_path_len
 ):
     file_exists = os.path.isfile(filename)
     with open(filename, mode='a', newline='') as csv_file:
@@ -25,14 +26,14 @@ def write_instance_set_res_to_csv(
                  "avg_acc", "avg_prec", "avg_rec", "avg_spec", "avg_f1", "avg_ano_link_perc", "avg_model_acc",
                  "gt_match_perc", "avg_ratio_of_found_gtfp", "avg_num_fault_paths", "max_num_fault_paths",
                  "avg_fault_path_len", "max_fault_path_len", "avg_runtime (s)", "avg_classification_ratio",
-                 "diag_success_percentage", "median_runtime (s)"]
+                 "diag_success_percentage", "median_runtime (s)", "median_num_fault_paths", "median_fault_path_len"]
             )
         instance_set_sol = instance_set_sol.split("/")[1].replace(".csv", "")
         writer.writerow([instance_set_sol, num_of_instances, avg_tp, avg_tn, avg_fp, avg_fn, fp_dev_mean, fp_dev_max,
                          avg_accuracy, avg_prec, avg_rec, avg_spec, avg_f1, avg_ano_link_percentage, avg_model_acc,
                          gt_match_percentage, avg_ratio_of_found_gtfp, avg_num_fault_paths, max_num_fault_paths,
                          avg_fault_path_len, max_fault_path_len, avg_runtime, avg_classification_ratio,
-                         diag_success_percentage, median_runtime])
+                         diag_success_percentage, median_runtime, median_num_fault_paths, median_fault_path_len])
 
 
 if __name__ == '__main__':
@@ -68,8 +69,10 @@ if __name__ == '__main__':
         diag_success_percentage = top_freq_diag_success if top_class_diag_success else 100 - top_freq_diag_success
 
         avg_num_fault_paths = round(df["#fault_paths"].describe()["mean"], 2)
+        median_num_fault_paths = round(df["#fault_paths"].median(), 2)
         max_num_fault_paths = df["#fault_paths"].describe()["max"]
         avg_fault_path_len = round(df["avg_fp_len"].describe()["mean"], 2)
+        median_fault_path_len = round(df["avg_fp_len"].median(), 2)
         max_fault_path_len = df["avg_fp_len"].describe()["max"]
 
         avg_runtime = round(df["runtime (s)"].describe()["mean"], 2)
@@ -82,5 +85,6 @@ if __name__ == '__main__':
             instance_set_sol, num_of_instances, avg_tp, avg_tn, avg_fp, avg_fn, fp_dev_mean, fp_dev_max, avg_accuracy,
             avg_prec, avg_rec, avg_spec, avg_f1, avg_ano_link_percentage, avg_model_acc, gt_match_percentage,
             avg_num_fault_paths, max_num_fault_paths, avg_fault_path_len, max_fault_path_len, filename, avg_runtime,
-            avg_classification_ratio, avg_ratio_of_found_gtfp, diag_success_percentage, median_runtime
+            avg_classification_ratio, avg_ratio_of_found_gtfp, diag_success_percentage, median_runtime,
+            median_num_fault_paths, median_fault_path_len
         )
