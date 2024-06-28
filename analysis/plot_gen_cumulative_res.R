a5748309b886d815aca0d260f74f5a23ce61063a
tbohne <tim.bohne@dfki.de>
2024-06-28 15:36:42 +0200

    refine plots layout -- apply compact instance names

--- analysis/plot_gen_cumulative_res.R
+++ analysis/plot_gen_cumulative_res.R
@@ -7,25 +7,23 @@ BAR_COLOR <- c(rgb(32, 43, 50, maxColorValue = 255))
 BAR_DEF <- geom_bar(stat = "identity", fill = BAR_COLOR, width = 0.75)
 COLOR_VALS <- c("#d44345", "#ffb641", "#ffff00", "#ccff99", "#00ff00")
 
-gen_multi_plot_four <- function(
-        pp1, pp2, pp3, pp4, y, x1, x2, x3, x4, filename
-    ) {
+gen_multi_plot_four <- function(pp1, pp2, pp3, pp4, y, x1, x2, x3, x4, filename) {
     fpp1 <- pp1 + BAR_DEF + coord_flip() + xlab(y) + ylab(x1) + scale_color_manual(values = color_mapping)
     fpp2 <- pp2 + BAR_DEF + coord_flip() + xlab(y) + ylab(x2) + scale_color_manual(values = color_mapping)
     fpp3 <- pp3 + BAR_DEF + coord_flip() + xlab(y) + ylab(x3) + scale_color_manual(values = color_mapping)
     fpp4 <- pp4 + BAR_DEF + coord_flip() + xlab(y) + ylab(x4) + scale_color_manual(values = color_mapping)
     combined_plot <- grid.arrange(fpp1, fpp2, fpp3, fpp4, ncol = 2)
-    ggsave(combined_plot, file = filename, width = 12, height = 6)
+    ggsave(combined_plot, file = filename, width = 12, height = 8)
 }
 
 gen_multi_plot_two <- function(pp1, pp2, y, x1, x2, filename) {
     fpp1 <- pp1 + BAR_DEF + coord_flip() + xlab(y) + ylab(x1) + scale_color_manual(values = color_mapping)
     fpp2 <- pp2 + BAR_DEF + coord_flip() + xlab(y) + ylab(x2) + scale_color_manual(values = color_mapping)
     combined_plot <- grid.arrange(fpp1, fpp2, ncol = 1)
-    ggsave(combined_plot, file = filename, width = 12, height = 6)
+    ggsave(combined_plot, file = filename, width = 12, height = 8)
 }
 
-input <- read.csv(file = "cumulative_res.csv", header = TRUE, sep = ",", check.name = FALSE)
+input <- read.csv(file = "compact_cumulative_res.csv", header = TRUE, sep = ",", check.name = FALSE)
 
 # instead of the percentage value factor(gt_match_perc) for which I'd need 100
 # colors, I only want to color / group them into 5 groups
