9842d175bb7c569bce4fad33aadb530d504d2029
tbohne <tim.bohne@dfki.de>
2024-07-08 14:30:07 +0200

    extend preprocessing for compact notation with instance-level res

--- analysis/preprocess_res.py
+++ analysis/preprocess_res.py
@@ -4,10 +4,26 @@
 
 import pandas as pd
 
+
+def create_compact_instance_notation_cumulative(data_frame):
+    for i in range(len(data_frame["instance_set"])):
+        e = data_frame["instance_set"][i].split("_")
+        data_frame.at[i, "instance_set"] = "_".join([e[1], e[2], e[5], e[6]])
+
+
+def create_compact_instance_notation_instance_level(data_frame):
+    for i in range(len(data_frame["instance"])):
+        e = data_frame["instance"][i].split("_")
+        # e.g., 129_10_20_50_10_95_99_42_64
+        data_frame.at[i, "instance"] = "_".join([e[1], e[2], e[5], e[6], e[8]])
+
+
+# cumulative res
 df = pd.read_csv("cumulative_res.csv")
-
-for i in range(len(df["instance_set"])):
-    e = df["instance_set"][i].split("_")
-    df.at[i, "instance_set"] = "_".join([e[1], e[2], e[5], e[6]])
-
+create_compact_instance_notation_cumulative(df)
 df.to_csv("compact_cumulative_res.csv", sep=',', encoding='utf-8', index=False)
+
+# instance set res
+df = pd.read_csv("res.csv")
+create_compact_instance_notation_instance_level(df)
+df.to_csv("compact_res.csv", sep=',', encoding='utf-8', index=False)
