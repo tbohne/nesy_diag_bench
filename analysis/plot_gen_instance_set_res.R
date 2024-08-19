98696e2330f5198d1fdafa38547bf13f5ab315a0
tbohne <tim.bohne@dfki.de>
2024-08-19 10:36:56 +0200

    refine fault path plot gen (inst lvl)

--- analysis/plot_gen_instance_set_res.R
+++ analysis/plot_gen_instance_set_res.R
@@ -91,24 +91,24 @@ gen_multi_plot_two_shared_x <- function(pp1, pp2, y, x1, x2, filename) {
 gen_fault_path_multi_plot <- function(
         pp1, pp2, pp3, pp4, y1, y2, y3, x1, x2, x3, x4, filename, group_name
     ) {
-    fpp1 <- pp1 + BAR_DEF + coord_flip() + xlab(y1) + ylab(x1) + COLOR_VALS + labs(color = group_name) + GENERAL_THEME
-    fpp2 <- pp2 + BAR_DEF + coord_flip() + xlab(NULL) + ylab(x2) + COLOR_VALS + labs(color = group_name) + SHARED_Y_THEME
-    fpp3 <- pp3 + geom_point(size=5) + xlab(x3) + ylab(y2) + COLOR_VALS + labs(color = group_name) + GENERAL_THEME
-    fpp4 <- pp4 + geom_point(size=5) + xlab(x4) + ylab(y3) + COLOR_VALS + labs(color = group_name) + GENERAL_THEME
+    fpp1 <- pp1 + BAR_DEF + xlab(y1) + ylab(x1) + COLOR_VALS + labs(color = group_name) + SHARED_X_THEME
+    fpp2 <- pp2 + BAR_DEF + xlab(y1) + ylab(x2) + COLOR_VALS + labs(color = group_name) + GENERAL_THEME_HORI
+    fpp3 <- pp3 + geom_point(size=3) + xlab(x3) + ylab(y2) + COLOR_VALS + labs(color = group_name) + GENERAL_THEME_HORI
+    fpp4 <- pp4 + geom_point(size=3) + xlab(x4) + ylab(y3) + COLOR_VALS + labs(color = group_name) + GENERAL_THEME_HORI
 
-    # extract legend from one of the plots (avoid redundant legends)
-    legend <- extract_legend(fpp1 + theme(legend.position = "bottom"))
+    # # extract legend from one of the plots (avoid redundant legends)
+    # legend <- extract_legend(fpp1 + theme(legend.position = "bottom"))
 
     combined_plot <- plot_grid(
-        fpp1, fpp2, fpp3, fpp4, ncol = 4, align = 'h', axis = 'h', rel_widths = c(0.85, 0.65, 0.65, 0.65)
+        fpp1, fpp2, fpp3, fpp4, nrow = 4, align = 'v', axis = 'v', rel_heights = c(0.65, 0.65, 0.65, 0.65)
     )
 
-    # add shared legend
-    final_plot <- plot_grid(
-        combined_plot, legend, ncol = 1, rel_heights = c(1, 0.05)
-    ) + theme(plot.background = element_rect(fill = "white", color = NA))
+    # # add shared legend
+    # final_plot <- plot_grid(
+    #     combined_plot, legend, ncol = 1, rel_heights = c(1, 0.05)
+    # ) + theme(plot.background = element_rect(fill = "white", color = NA))
 
-    ggsave(final_plot, file = filename, width = 18, height = 20)
+    ggsave(combined_plot, file = filename, width = 15, height = 7)
 }
 
 input <- read.csv(file = "compact_res.csv", header = TRUE, sep = ",", check.name = FALSE)
@@ -126,13 +126,13 @@ input$instance_suffix <- factor(
 
 p1 <- ggplot(
     data = input, aes_string(
-        x = "instance", y = "avg_fp_len", color = "gt_match", group = "gt_match"
+        x = "instance_suffix", y = "avg_fp_len", color = "gt_match", group = "gt_match"
     )
 )
 
 p2 <- ggplot(
     data = input, aes_string(
-        x = "instance", y = "`#fault_paths`", color = "gt_match", group = "gt_match"
+        x = "instance_suffix", y = "`#fault_paths`", color = "gt_match", group = "gt_match"
     )
 )
 
@@ -156,9 +156,9 @@ gen_fault_path_multi_plot(
     TeX("$l^a_{i_\\eta}$"),
     TeX("runtime (s)"),
     TeX("$l^a_{i_\\eta}$"),
-    "num of fault paths",
-    "num of fault paths",
-    "num of fault paths",
+    "num fault paths",
+    "num fault paths",
+    "num fault paths",
     "fault_paths.png",
     TeX("$p_2$")
 )
