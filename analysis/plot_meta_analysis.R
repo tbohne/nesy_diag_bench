41d9a8310920ad08a42342806a24eeb8e730e143
tbohne <tim.bohne@dfki.de>
2024-08-22 17:05:24 +0200

    refine notations of meta analysis plots

--- analysis/plot_meta_analysis.R
+++ analysis/plot_meta_analysis.R
@@ -138,20 +138,20 @@ p6 <- ggplot(
 
 gen_multi_plot_six(
     p1, p2, p3, p4, p5, p6,
-    TeX("$p_1$"), # y1
+    TeX("$\\bar{p_1}^i$"), # y1
     TeX("$\\alpha / \\beta$"), # y2
     TeX("$\\alpha / \\beta$"), # y3
     TeX("$\\alpha / \\beta$"), # y4
-    TeX("$|p_1 - F1|$"), # y5
+    TeX("$|\\bar{p_1}^i - \\bar{F1}|$"), # y5
     TeX("$\\alpha / \\beta$"), # y6
-    TeX("$p_0$"), # x1
-    TeX("$|p_0 - F1|$"), # x2
-    TeX("$p_0$"), # x3
-    TeX("$p_1$"), # x4
-    TeX("$|p_0 - F1|$"), # x5
-    TeX("$|p_1 - F1|$"), # x6
+    TeX("$\\bar{p_0}^i$"), # x1
+    TeX("$|\\bar{p_0}^i - \\bar{F1}|$"), # x2
+    TeX("$\\bar{p_0}^i$"), # x3
+    TeX("$\\bar{p_1}^i$"), # x4
+    TeX("$|\\bar{p_0}^i - \\bar{F1}|$"), # x5
+    TeX("$|\\bar{p_1}^i - \\bar{F1}|$"), # x6
     "compensation_analysis.png",
-    TeX("$p_2$")
+    TeX("$p_2^i$")
 )
 
 # same with prod #############################
@@ -212,20 +212,20 @@ p6 <- ggplot(
 
 gen_multi_plot_six(
     p1, p2, p3, p4, p5, p6,
-    TeX("$p_1$"), # y1
+    TeX("$\\bar{p_1}^i$"), # y1
     TeX("$\\alpha \\beta$"), # y2
     TeX("$\\alpha \\beta$"), # y3
     TeX("$\\alpha \\beta$"), # y4
-    TeX("$|p_1 - F1|$"), # y5
+    TeX("$|\\bar{p_1}^i - \\bar{F1}|$"), # y5
     TeX("$\\alpha \\beta$"), # y6
-    TeX("$p_0$"), # x1
-    TeX("$|p_0 - F1|$"), # x2
-    TeX("$p_0$"), # x3
-    TeX("$p_1$"), # x4
-    TeX("$|p_0 - F1|$"), # x5
-    TeX("$|p_1 - F1|$"), # x6
+    TeX("$\\bar{p_0}^i$"), # x1
+    TeX("$|\\bar{p_0}^i - \\bar{F1}|$"), # x2
+    TeX("$\\bar{p_0}^i$"), # x3
+    TeX("$\\bar{p_1}^i$"), # x4
+    TeX("$|\\bar{p_0}^i - \\bar{F1}|$"), # x5
+    TeX("$|\\bar{p_1}^i - \\bar{F1}|$"), # x6
     "compensation_analysis_prod.png",
-    TeX("$p_2$")
+    TeX("$p_2^i$")
 )
 
 ##############################################
@@ -286,20 +286,20 @@ p6 <- ggplot(
 
 gen_multi_plot_six(
     p1, p2, p3, p4, p5, p6,
-    TeX("$p_0$"), # y1
-    TeX("$p_1$"), # y2
-    TeX("$n_c$"), # y3
+    TeX("$\\bar{p_0}^i$"), # y1
+    TeX("$\\bar{p_1}^i$"), # y2
+    TeX("$\\bar{n_c}^i$"), # y3
     TeX("$f^a_i + \\tilde{f^a_i}$"), # y4
     TeX("$f^a_i + \\tilde{f^a_i}$"), # y5
     TeX("$f^a_i + \\tilde{f^a_i}$"), # y6
-    TeX("$F1$"), # x1
-    TeX("$F1$"), # x2
-    TeX("$F1$"), # x3
-    TeX("$p_2$"), # x4
-    TeX("$p_1$"), # x5
-    TeX("$p_0$"), # x6
+    TeX("$\\bar{F1}$"), # x1
+    TeX("$\\bar{F1}$"), # x2
+    TeX("$\\bar{F1}$"), # x3
+    TeX("$p_2^i$"), # x4
+    TeX("$\\bar{p_1}^i$"), # x5
+    TeX("$\\bar{p_0}^i$"), # x6
     "f1_end_res_corr.png",
-    TeX("$p_2$")
+    TeX("$p_2^i$")
 )
 
 # model acc - connectivity - performance
@@ -366,14 +366,14 @@ gen_multi_plot_six(
     TeX("$\\gamma / \\beta $"), # y4
     TeX("$\\gamma / \\beta $"), # y5
     TeX("$\\gamma / \\beta $"), # y6
-    TeX("$p_0$"), # x1
-    TeX("$p_1$"), # x2
-    TeX("$p_2$"), # x3
-    TeX("$d_s$"), # x4
+    TeX("$\\bar{p_0}^i$"), # x1
+    TeX("$\\bar{p_1}^i$"), # x2
+    TeX("$p_2^i$"), # x3
+    TeX("$d_s^i$"), # x4
     TeX("$\\tilde{f^a_i}$"), # x5
     TeX("$\\tilde{f^{max}_i}$"), # x6
     "modelacc_connectivity_performance.png",
-    TeX("$p_2$")
+    TeX("$p_2^i$")
 )
 
 # how many classifications and how acc models
@@ -434,20 +434,20 @@ p6 <- ggplot(
 
 gen_multi_plot_six(
     p1, p2, p3, p4, p5, p6,
-    TeX("$n_c / \\gamma$"), # y1
-    TeX("$n_c / \\gamma$"), # y2
-    TeX("$n_c / \\gamma$"), # y3
-    TeX("$n_c / \\gamma$"), # y4
-    TeX("$n_c / \\gamma$"), # y5
-    TeX("$n_c / \\gamma$"), # y6
-    TeX("$p_0$"), # x1
-    TeX("$p_1$"), # x2
-    TeX("$p_2$"), # x3
-    TeX("$d_s$"), # x4
+    TeX("$\\bar{n_c}^i / \\gamma$"), # y1
+    TeX("$\\bar{n_c}^i / \\gamma$"), # y2
+    TeX("$\\bar{n_c}^i / \\gamma$"), # y3
+    TeX("$\\bar{n_c}^i / \\gamma$"), # y4
+    TeX("$\\bar{n_c}^i / \\gamma$"), # y5
+    TeX("$\\bar{n_c}^i / \\gamma$"), # y6
+    TeX("$\\bar{p_0}^i$"), # x1
+    TeX("$\\bar{p_1}^i$"), # x2
+    TeX("$p_2^i$"), # x3
+    TeX("$d_s^i$"), # x4
     TeX("$\\tilde{f^a_i}$"), # x5
     TeX("$\\tilde{f^{max}_i}$"), # x6
     "num_classifications_model_acc.png",
-    TeX("$p_2$")
+    TeX("$p_2^i$")
 )
 
 # affected_by against perf
@@ -514,14 +514,14 @@ gen_multi_plot_six(
     TeX("$\\beta$"), # y4
     TeX("$\\beta$"), # y5
     TeX("$\\beta$"), # y6
-    TeX("$p_0$"), # x1
-    TeX("$p_1$"), # x2
-    TeX("$p_2$"), # x3
-    TeX("$d_s$"), # x4
+    TeX("$\\bar{p_0}^i$"), # x1
+    TeX("$\\bar{p_1}^i$"), # x2
+    TeX("$p_2^i$"), # x3
+    TeX("$d_s^i$"), # x4
     TeX("$\\tilde{f^a_i}$"), # x5
     TeX("$\\tilde{f^{max}_i}$"), # x6
     "aff_by_perf.png",
-    TeX("$p_2$")
+    TeX("$p_2^i$")
 )
 
 # model_acc against perf
@@ -588,14 +588,14 @@ gen_multi_plot_six(
     TeX("$\\gamma$"), # y4
     TeX("$\\gamma$"), # y5
     TeX("$\\gamma$"), # y6
-    TeX("$p_0$"), # x1
-    TeX("$p_1$"), # x2
-    TeX("$p_2$"), # x3
-    TeX("$d_s$"), # x4
+    TeX("$\\bar{p_0}^i$"), # x1
+    TeX("$\\bar{p_1}^i$"), # x2
+    TeX("$p_2^i$"), # x3
+    TeX("$d_s^i$"), # x4
     TeX("$\\tilde{f^a_i}$"), # x5
     TeX("$\\tilde{f^{max}_i}$"), # x6
     "model_acc_perf.png",
-    TeX("$p_2$")
+    TeX("$p_2^i$")
 )
 
 # anomaly_perc against perf
@@ -736,14 +736,14 @@ gen_multi_plot_six(
     TeX("$\\alpha / \\gamma$"), # y4
     TeX("$\\alpha / \\gamma$"), # y5
     TeX("$\\alpha / \\gamma$"), # y6
-    TeX("$p_0$"), # x1
-    TeX("$p_1$"), # x2
-    TeX("$p_2$"), # x3
-    TeX("$d_s$"), # x4
+    TeX("$\\bar{p_0}^i$"), # x1
+    TeX("$\\bar{p_1}^i$"), # x2
+    TeX("$p_2^i$"), # x3
+    TeX("$d_s^i$"), # x4
     TeX("$\\tilde{f^a_i}$"), # 5
     TeX("$\\tilde{f^{max}_i}$"), # x6
     "anomaly_perc_model_acc_ratio_perf.png",
-    TeX("$p_2$")
+    TeX("$p_2^i$")
 )
 
 # num classifications against perf
@@ -901,15 +901,15 @@ gen_multi_plot_six(
     TeX("$\\gamma$"), #y4
     TeX("$\\gamma$"), #y5
     TeX("$\\gamma$"), #y6
-    TeX("$n_c$"), #x1
+    TeX("$\\bar{n_c}^i$"), #x1
     # filtered based on diff p1 thresholds
-    TeX("$n_c (p_1 \\geq 0.7)$"), #x2
-    TeX("$n_c (p_1 \\geq 0.75)$"), #x3
-    TeX("$n_c (p_1 \\geq 0.8)$"), #x4
-    TeX("$n_c (p_1 \\geq 0.9)$"), #x5
-    TeX("$n_c (\\max p_1)$"), #x6
+    TeX("$\\bar{n_c}^i (\\bar{p_1}^i \\geq 0.7)$"), #x2
+    TeX("$\\bar{n_c}^i (\\bar{p_1}^i \\geq 0.75)$"), #x3
+    TeX("$\\bar{n_c}^i (\\bar{p_1}^i \\geq 0.8)$"), #x4
+    TeX("$\\bar{n_c}^i (\\bar{p_1}^i \\geq 0.9)$"), #x5
+    TeX("$\\bar{n_c}^i (\\max \\bar{p_1}^i)$"), #x6
     "model_acc_classifications.png",
-    TeX("$p_2$")
+    TeX("$p_2^i$")
 )
 
 # num classifications - model acc -- filtered based on anomaly_link_perc_scores
@@ -993,14 +993,14 @@ gen_multi_plot_six(
     TeX("$\\gamma$"), #y4
     TeX("$\\gamma$"), #y5
     TeX("$\\gamma$"), #y6
-    TeX("$n_c$"), #x1
-    TeX("$n_c (p_0 \\geq 0.7)$"), #x2
-    TeX("$n_c (p_0 \\geq 0.75)$"), #x3
-    TeX("$n_c (p_0 \\geq 0.8)$"), #x4
-    TeX("$n_c (p_0 \\geq 0.9)$"), #x5
-    TeX("$n_c (\\max p_0)$"), #x6
+    TeX("$\\bar{n_c}^i$"), #x1
+    TeX("$\\bar{n_c}^i (\\bar{p_0}^i \\geq 0.7)$"), #x2
+    TeX("$\\bar{n_c}^i (\\bar{p_0}^i \\geq 0.75)$"), #x3
+    TeX("$\\bar{n_c}^i (\\bar{p_0}^i \\geq 0.8)$"), #x4
+    TeX("$\\bar{n_c}^i (\\bar{p_0}^i \\geq 0.9)$"), #x5
+    TeX("$\\bar{n_c}^i (\\max \\bar{p_0}^i)$"), #x6
     "model_acc_classifications_ano_link.png",
-    TeX("$p_2$")
+    TeX("$p_2^i$")
 )
 
 # num classifications - model acc -- filtered based on gt_match_perc
@@ -1084,14 +1084,14 @@ gen_multi_plot_six(
     TeX("$\\gamma$"), #y4
     TeX("$\\gamma$"), #y5
     TeX("$\\gamma$"), #y6
-    TeX("$n_c$"), #x1
-    TeX("$n_c (p_2 \\geq 0.7)$"), #x2
-    TeX("$n_c (p_2 \\geq 0.75)$"), #x3
-    TeX("$n_c (p_2 \\geq 0.8)$"), #x4
-    TeX("$n_c (p_2 \\geq 0.9)$"), #x5
-    TeX("$n_c (\\max p_2)$"), #x6
+    TeX("$\\bar{n_c}^i$"), #x1
+    TeX("$\\bar{n_c}^i (p_2^i \\geq 0.7)$"), #x2
+    TeX("$\\bar{n_c}^i (p_2^i \\geq 0.75)$"), #x3
+    TeX("$\\bar{n_c}^i (p_2^i \\geq 0.8)$"), #x4
+    TeX("$\\bar{n_c}^i (p_2^i \\geq 0.9)$"), #x5
+    TeX("$\\bar{n_c}^i (\\max p_2^i)$"), #x6
     "model_acc_classifications_gt_match_perc.png",
-    TeX("$p_2$")
+    TeX("$p_2^i$")
 )
 
 # correlation affected-by and fault path len / num fault paths
@@ -1162,10 +1162,10 @@ gen_multi_plot_six(
     TeX("$l^a_i$"), # x2
     TeX("$f^a_i$"), # x3
     TeX("$l^a_i$"), # x4
-    TeX("$p_1$"), # x5
+    TeX("$\\bar{p_1}^i$"), # x5
     TeX("$l^a_i$"), # x6
     "correlation_anomaly_perc_aff_by_prod_fault_paths.png",
-    TeX("$p_2$")
+    TeX("$p_2^i$")
 )
 
 # correlation (median + mean) runtime and fault path len / num fault paths
@@ -1239,7 +1239,7 @@ gen_multi_plot_six(
     TeX("$l^m_i$"), # x5
     TeX("$f^m_i$"), # x6
     "correlation_runtime_fault_paths.png",
-    TeX("$p_2$")
+    TeX("$p_2^i$")
 )
 
 # correlation (median + mean + max) runtime and sum of (mean + max) fp + fp dev
@@ -1313,7 +1313,7 @@ gen_multi_plot_six(
     TeX("$f^{max}_i + \\tilde{f^{max}_i}$"), # x5
     TeX("$f^{max}_i + \\tilde{f^{max}_i}$"), # x6
     "correlation_runtime_fault_paths_sum.png",
-    TeX("$p_2$")
+    TeX("$p_2^i$")
 )
 
 # correlation alpha, beta, gamma vs. FPs
@@ -1381,7 +1381,7 @@ gen_multi_plot_six(
     TeX("$\\gamma$"), # y3
     TeX("$(\\alpha + \\beta) / \\gamma$"), # y4
     TeX("$(\\alpha + \\beta) / \\gamma$"), # y5
-    TeX("$n_c / \\gamma$"), # y6
+    TeX("$\\bar{n_c}^i / \\gamma$"), # y6
     TeX("$\\bar{FP}$"), # x1
     TeX("$\\bar{FP}$"), # x2
     TeX("$\\bar{FP}$"), # x3
@@ -1389,7 +1389,7 @@ gen_multi_plot_six(
     TeX("$\\bar{FP} (\\gamma \\neq 1.0)$"), # x5
     TeX("$\\bar{FP}$"), # x6
     "anomaly_perc_aff_by_model_acc_aggregation.png",
-    TeX("$p_2$")
+    TeX("$p_2^i$")
 )
 
 # correlation alpha, beta, gamma vs. FNs
@@ -1457,7 +1457,7 @@ gen_multi_plot_six(
     TeX("$\\gamma$"), # y3
     TeX("$\\alpha / \\gamma$"), # y4
     TeX("$\\alpha / \\gamma$"), # y5
-    TeX("$n_c / \\gamma$"), # y6
+    TeX("$\\bar{n_c}^i / \\gamma$"), # y6
     TeX("$\\bar{FN}$"), # x1
     TeX("$\\bar{FN}$"), # x2
     TeX("$\\bar{FN}$"), # x3
@@ -1465,7 +1465,7 @@ gen_multi_plot_six(
     TeX("$\\bar{FN} (\\gamma \\neq 1.0)$"), # x5
     TeX("$\\bar{FN}$"), # x6
     "anomaly_perc_aff_by_model_acc_aggregation_fn.png",
-    TeX("$p_2$")
+    TeX("$p_2^i$")
 )
 
 # correlation missed anomalies vs. performance
@@ -1526,20 +1526,20 @@ p6 <- ggplot(
 
 gen_multi_plot_six(
     p1, p2, p3, p4, p5, p6,
-    TeX("$p_0$"), # y1
-    TeX("$p_0$"), # y2
-    TeX("$p_0$"), # y3
-    TeX("$p_1$"), # y4
-    TeX("$p_1$"), # y5
-    TeX("$p_1$"), # y6
-    TeX("$m_3$"), # x1
-    TeX("$m_2$"), # x2
-    TeX("$m_1$"), # x3
-    TeX("$m_3$"), # x4
-    TeX("$m_2$"), # x5
-    TeX("$m_1$"), # x6
+    TeX("$\\bar{p_0}^i$"), # y1
+    TeX("$\\bar{p_0}^i$"), # y2
+    TeX("$\\bar{p_0}^i$"), # y3
+    TeX("$\\bar{p_1}^i$"), # y4
+    TeX("$\\bar{p_1}^i$"), # y5
+    TeX("$\\bar{p_1}^i$"), # y6
+    TeX("$\\bar{m_3}^i$"), # x1
+    TeX("$\\bar{m_2}^i$"), # x2
+    TeX("$\\bar{m_1}^i$"), # x3
+    TeX("$\\bar{m_3}^i$"), # x4
+    TeX("$\\bar{m_2}^i$"), # x5
+    TeX("$\\bar{m_1}^i$"), # x6
     "correlation_missed_anomalies_perf.png",
-    TeX("$p_2$")
+    TeX("$p_2^i$")
 )
 
 # correlation compensation metrics
@@ -1603,17 +1603,17 @@ gen_multi_plot_six(
     TeX("$\\beta$"), # y1
     TeX("$\\beta$"), # y2
     TeX("$\\beta$"), # y3
-    TeX("$\\bar{c_1}$"), # y4
-    TeX("$\\bar{c_1}$"), # y5
-    TeX("$\\bar{c_2}$"), # y6
-    TeX("$\\bar{c_1}$"), # x1
-    TeX("$\\bar{c_2}$"), # x2
-    TeX("$\\bar{c_3}$"), # x3
-    TeX("$p_0$"), # x4
-    TeX("$p_1$"), # x5
-    TeX("$p_1$"), # x6
+    TeX("$\\bar{c_1}^i$"), # y4
+    TeX("$\\bar{c_1}^i$"), # y5
+    TeX("$\\bar{c_2}^i$"), # y6
+    TeX("$\\bar{c_1}^i$"), # x1
+    TeX("$\\bar{c_2}^i$"), # x2
+    TeX("$\\bar{c_3}^i$"), # x3
+    TeX("$\\bar{p_0}^i$"), # x4
+    TeX("$\\bar{p_1}^i$"), # x5
+    TeX("$\\bar{p_1}^i$"), # x6
     "correlation_compensation_metrics.png",
-    TeX("$p_2$")
+    TeX("$p_2^i$")
 )
 
 # correlation compensation metrics (with alpha=0.2)
@@ -1678,17 +1678,17 @@ gen_multi_plot_six(
     TeX("$\\beta$"), # y1
     TeX("$\\beta$"), # y2
     TeX("$\\beta$"), # y3
-    TeX("$\\bar{c_1}$"), # y4
-    TeX("$\\bar{c_1}$"), # y5
-    TeX("$\\bar{c_2}$"), # y6
-    TeX("$\\bar{c_1}$"), # x1
-    TeX("$\\bar{c_2}$"), # x2
-    TeX("$\\bar{c_3}$"), # x3
-    TeX("$p_0$"), # x4
-    TeX("$p_1$"), # x5
-    TeX("$p_1$"), # x6
+    TeX("$\\bar{c_1}^i$"), # y4
+    TeX("$\\bar{c_1}^i$"), # y5
+    TeX("$\\bar{c_2}^i$"), # y6
+    TeX("$\\bar{c_1}^i$"), # x1
+    TeX("$\\bar{c_2}^i$"), # x2
+    TeX("$\\bar{c_3}^i$"), # x3
+    TeX("$\\bar{p_0}^i$"), # x4
+    TeX("$\\bar{p_1}^i$"), # x5
+    TeX("$\\bar{p_1}^i$"), # x6
     "correlation_compensation_metrics_alpha20.png",
-    TeX("$p_2$")
+    TeX("$p_2^i$")
 )
 
 # "best of" correlation plots
@@ -1842,11 +1842,11 @@ gen_multi_plot_six(
     TeX("$\\beta$"), # y5
     TeX("$\\beta$"), # y6
     TeX("$\\bar{FP} + \\bar{FN} (\\alpha \\geq 0.2, \\gamma \\in \\[0.95, 0.99\\])$"), # x1
-    TeX("$p_0 (\\alpha \\geq 0.2, \\gamma \\in \\[0.95, 0.99\\])$"), # x2
-    TeX("$p_1 (\\alpha \\geq 0.2, \\gamma \\in \\[0.95, 0.99\\])$"), # x3
-    TeX("$p_2 (\\alpha \\geq 0.2, \\gamma \\in \\[0.95, 0.99\\])$"), # x4
-    TeX("$\\bar{c_1} (\\alpha \\geq 0.2, \\gamma \\in \\[0.95, 0.99\\])$"), # x5
-    TeX("$\\bar{c_2} (\\alpha \\geq 0.2, \\gamma \\in \\[0.95, 0.99\\])$"), # x6
+    TeX("$\\bar{p_0}^i (\\alpha \\geq 0.2, \\gamma \\in \\[0.95, 0.99\\])$"), # x2
+    TeX("$\\bar{p_1}^i (\\alpha \\geq 0.2, \\gamma \\in \\[0.95, 0.99\\])$"), # x3
+    TeX("$p_2^i (\\alpha \\geq 0.2, \\gamma \\in \\[0.95, 0.99\\])$"), # x4
+    TeX("$\\bar{c_1}^i (\\alpha \\geq 0.2, \\gamma \\in \\[0.95, 0.99\\])$"), # x5
+    TeX("$\\bar{c_2}^i (\\alpha \\geq 0.2, \\gamma \\in \\[0.95, 0.99\\])$"), # x6
     "beta_influence.png",
-    TeX("$p_2$")
+    TeX("$p_2^i$")
 )
